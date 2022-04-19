dc518505aef7d8ff28c8340f65a47e61098809c2
SpirosNatsios <spyrosodnut@gmail.com>
2022-04-19 22:20:17 +0300

    added backspace button

--- script.js
+++ script.js
@@ -1,3 +1,73 @@
+let num1 = undefined;
+let operator = "s";
+let operation = true;
+let isOperating = false;
+const display = document.querySelector(".display");
+const numButtons = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+const backSpace = document.querySelector("#backspase");
+const dot = document.querySelector("#dot");
+
+operators.forEach((operators) => {
+  operators.addEventListener("click", (e) => {
+    if (isOperating) equality();
+    operator = e.target.textContent;
+    operation = true;
+    isOperating = true;
+    num1 = display.textContent;
+  });
+});
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (operation) {
+      display.textContent = "";
+      operation = false;
+    }
+    if (display.textContent.length >= 10) return;
+    display.textContent += e.target.textContent;
+  });
+});
+
+equal.addEventListener("click", () => {
+  if (num1 == undefined) {
+    clearScreen();
+    display.textContent = "Error";
+    return;
+  }
+  equality();
+  operator = "";
+});
+
+function equality() {
+  num2 = display.textContent;
+  display.textContent = operate(operator, +num1, +num2);
+  operation = true;
+  isOperating = false;
+  num1 = undefined;
+}
+
+clear.addEventListener("click", clearScreen);
+
+function clearScreen() {
+  display.textContent = "0";
+  operator = "";
+  operation = true;
+  isOperating = false;
+}
+
+backSpace.addEventListener("click", () => {
+  let displayScreen = display.textContent;
+  if (displayScreen == "0") return;
+  display.textContent = displayScreen.slice(0, -1);
+  if (displayScreen.length == 1) {
+    display.textContent = "0";
+    operation = true;
+  }
+});
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -27,83 +97,3 @@ function operate(operator, num1, num2) {
       return divide(num1, num2);
   }
 }
-let operator = "";
-let operation = true;
-let isOperating = false;
-const display = document.querySelector(".display");
-const numButtons = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operator");
-const equal = document.querySelector("#equal");
-const plus = document.querySelector("#plus");
-const minus = document.querySelector("#minus");
-const multiplier = document.querySelector("#multiply");
-const divider = document.querySelector("#divide");
-const clear = document.querySelector("#clear");
-
-plus.addEventListener("click", () => {
-  if (isOperating) equality();
-  operator = "+";
-  operation = true;
-  isOperating = true;
-  num1 = display.textContent;
-  console.log(num1);
-});
-
-minus.addEventListener("click", () => {
-  if (isOperating) equality();
-  operator = "-";
-  operation = true;
-  isOperating = true;
-  num1 = display.textContent;
-  console.log(num1);
-});
-
-multiplier.addEventListener("click", () => {
-  if (isOperating) equality();
-  operator = "*";
-  operation = true;
-  isOperating = true;
-  num1 = display.textContent;
-  console.log(num1);
-});
-
-divider.addEventListener("click", () => {
-  if (isOperating) equality();
-  operator = "/";
-  operation = true;
-  isOperating = true;
-  num1 = display.textContent;
-  console.log(num1);
-});
-
-numButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (operation) {
-      display.textContent = "";
-      operation = false;
-    }
-    display.textContent += e.target.textContent;
-  });
-});
-
-equal.addEventListener("click", () => {
-  equality();
-  operator = "";
-});
-
-function equality() {
-  num2 = display.textContent;
-  console.log(num1);
-  console.log(num2);
-  console.log(operator);
-  display.textContent = operate(operator, +num1, +num2);
-  operation = true;
-  isOperating = false;
-}
-
-clear.addEventListener("click", () => {
-  display.textContent = "0";
-  operator = "";
-  operation = true;
-  isOperating = false;
-});
